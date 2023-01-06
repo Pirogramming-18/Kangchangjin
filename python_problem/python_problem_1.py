4dc25f2915f23e83cc64f728844c180bdbcc7a84
cj <toong1203@naver.com>
2023-01-06 16:31:47 +0900

    step8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,27 +1,35 @@
-import random
-num=0
 
-player_change=0 #a는 0 / b는 1
-while True:
-    try:
-        counting=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-        if not 1<=counting<=3:
-            raise Exception('1,2,3 중 하나를 입력하세요')
-    except ValueError:
-        print('정수를 입력하세요')    
-    except Exception as e:
-        print(e)
-    
-    if player_change==0:
-        num+=counting
-        player_change=1
-        if num>=31:
-            print('playerB win!')
-            break
+def brGame():
+    num=0
+    player_change=0 #a는 0 / b는 1
 
-    elif player_change==1:
-        num+=counting
-        player_change=0
-        if num>=31:
-            print('playerA win!')
-            break
+    while True:
+        try:
+            counting=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+            if not 1<=counting<=3:
+                raise Exception('1,2,3 중 하나를 입력하세요')
+        except ValueError:
+            print('정수를 입력하세요')    
+        except Exception as e:
+            print(e)
+
+        if player_change==0:
+            num+=counting
+            player_change=1
+            if num>=31:
+                # print('playerB win!')
+                return player_change
+                
+
+        elif player_change==1:
+            num+=counting
+            player_change=0
+            if num>=31:
+                # print('playerA win!')
+                return player_change
+            
+br=brGame()
+if br==0:
+    print('playerA win!')
+elif br==1:
+    print('playerB win!')
