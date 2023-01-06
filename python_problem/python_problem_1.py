82f6a7ec9b9b9675e7e294914f1b5eaadc98e4d5
cj <toong1203@naver.com>
2023-01-06 12:34:43 +0900

    step3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,14 @@
+import random
 num=0
-counting=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+# counting=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
 
-print(counting)
+while True:
+    try:
+        counting=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+        if not 1<=counting<=3:
+            raise Exception
+        break
+    except ValueError:
+        print('정수를 입력하세요')    
+    except Exception as notrightnum:
+        print('1,2,3 중 하나를 입력하세요')
