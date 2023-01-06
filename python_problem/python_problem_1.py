523fc0c227684d00c153a26e361cd3d4e98255a4
cj <toong1203@naver.com>
2023-01-06 14:56:44 +0900

    step5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,18 +1,23 @@
 import random
 num=0
-# counting=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
 
 while True:
     try:
-        counting=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-        if not 1<=counting<=3:
-            raise Exception
-        break
+        counting_a=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+        if not 1<=counting_a<=3:
+            raise Exception('1,2,3 중 하나를 입력하세요')
+        # break
+        counting_b=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+        if not 1<=counting_b<=3:
+            raise Exception('1,2,3 중 하나를 입력하세요')
+        # break
     except ValueError:
         print('정수를 입력하세요')    
-    except Exception as notrightnum:
-        print('1,2,3 중 하나를 입력하세요')
-
-while num<counting:
-    num+=1
-    print('playerA : ',format(num))
+    except Exception as e:
+        print(e)
+    num+=counting_a
+    counting_all=counting_a+counting_b
+    while num<counting_all:
+        num+=1
+        print('playerB : ',num)
+    break
