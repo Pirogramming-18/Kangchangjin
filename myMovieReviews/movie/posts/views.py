38a44eec583a3136af23690a54f41ed5b201d2f7
cj <toong1203@naver.com>
2023-01-13 03:48:07 +0900

    home 화면 html 진행중
    기능1

--- myMovieReviews/movie/posts/views.py
+++ myMovieReviews/movie/posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
+# from django.http.request import HttpRequest
 # Create your views here.
+
+def home(request):
+    posts=Post.objects.all()
+    return render(request, "posts/home.html",{"posts":posts})
